e72c471a7101231b29f1e17316a7aeb5879f5095
Ernesto Celis <celisdelafuente@gmail.com>
2017-12-22 00:04:13 -0600

    ADD qualifiers.py

---
+++ sally/qualifiers.py
@@ -0,0 +1,44 @@
+
+QUALIFIER = {
+        'products': [
+            'cosméticos',
+            'medicina',
+            'tinte*cabello',
+            'mujer',
+            'bolsos',
+            'libros',
+            'uñas',
+            'shampoo',
+            'lentes',
+            'sandálias',
+            'mascotas',
+            'muebles',
+            'equipos',
+            'maquinaria',
+            'motores',
+            'desechables',
+            'instrumentos musicales',
+            'computo',
+            'electrónica',
+            'ropa',
+            'autopartes',
+            'ferreteria',
+            'sex shop',
+            'fotocopiadoras',
+            'zapatos',
+            'bisuteria',
+            'joyeria', # TODO more products
+            ],
+        'services': [
+            'envíos', # TODO more services
+            ],
+        'ecommerce': [
+            'woocommerce',
+            'shopify',
+            'magento',
+            'prestashop',
+            'shoperti'
+            ]
+        }
+
+
