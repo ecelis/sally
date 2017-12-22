67d74417ec861d47707853530ee0745b9dcafe99
Ernesto Celis <celisdelafuente@gmail.com>
2017-12-22 11:15:34 -0600

    FIX flags must be None

--- sally/spreadsheets.py
+++ sally/spreadsheets.py
@@ -8,11 +8,12 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+#try:
+#    import argparse
+#    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+#except ImportError:
+#    flags = None
+flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
