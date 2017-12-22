e0dd7f349af261f6adeb6d250d9b1508cfb5af27
Ernesto Celis <celisdelafuente@gmail.com>
2017-12-22 00:17:44 -0600

    FIXes as junkie

--- sally/items.py
+++ sally/items.py
@@ -6,7 +6,7 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-import qualifiers as q
+import sally.qualifiers as q
 
 class SallyItem(scrapy.Item):
     # define the fields for your item here like:
