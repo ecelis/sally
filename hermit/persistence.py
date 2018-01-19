944aaeaba5daea4b86ebbc572822876c903a6984
Ernesto Celis <celisdelafuente@gmail.com>
2018-01-19 15:10:48 -0600

    ADD mongo persistence for hermit

---
+++ hermit/persistence.py
@@ -0,0 +1,15 @@
+import os
+from mongoengine import connect
+
+
+class HermitMongo:
+
+    def __init__(self):
+        connect('sally', host=os.environ.get('MONGO_HOST',
+            username=os.environ.get('MONGO_USER'),
+            password=os.environ.get('MONGO_PASSWORD')))
+
+class User(Document):
+    email = StringField(required=True)
+    fb_userId = StringField(required=True)
+    fb_accessToken = StringField(requied=True)
