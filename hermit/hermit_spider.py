fab96184e7156fb590f98a486528ed71e47403b3
Ernesto Celis <celisdelafuente@gmail.com>
2018-01-24 12:35:14 -0600

    WIP FB scrap getting the good links, ignore user accounts by now

--- hermit/hermit_spider.py
+++ hermit/hermit_spider.py
@@ -19,19 +19,47 @@ class HermitCrab(object):
         self.score = gs.get_score()
         self.fb_user_id = fb_user_id
         self.access_token = self.get_token()
-        print(self.access_token)
         self.graph = 'https://graph.facebook.com'
+        self.sheet_rows = [
+                ['SCORE','WEB SITE', 'OFFER', 'META', 'TELPHONE', 'EMAIL',
+                'ECOMMERCE','SHOPPING CART', 'SOCIAL NETWORKS' 'PLACE', 'CRAWL DATE']
+                ]
 
         lines = ["%s" % str(l).rstrip() for l in gs.get_urls(source_file)]
         fb = re.compile(r'facebook', re.IGNORECASE)
         self.start_urls = list(filter(fb.search, list(filter(None, lines))))
 
-        print(self.start_urls)
-        print(len(self.start_urls))
-
         for item in self.start_urls:
-            print(item.split('/')[1])
-            self.parse_item(item.split('/')[1])
+            response = self.parse_item(item.split('/')[1])
+            if 'error' in response:
+#                print(response['error']['message'])
+                continue
+            else:
+                if 'location' in response:
+                    city = response['location']['city'] if 'city' in response['location'] else None
+                    street = response['location']['street'] if 'street' in response['location'] else ''
+                    zip_code = response['location']['zip'] if 'zip' in response['location'] else ''
+                    address = street +', '+zip_code
+                    country = response['location']['country'] if 'country' in response['location'] else None
+                else:
+                    city = None
+                    address = None
+                    country = None
+
+                row = [
+                        0,
+                        "https://www.facebook.com/%s" % item,
+                        response['about'] if'about' in response else None,
+                        response['category'] if 'category' in response else None,
+                        response['engagement']['count'] if 'engagement' in response else None,
+                        response['phone'] if 'phone' in response else None,
+                        ','.join(response['emails']) if 'emails' in response else None,
+                        address,
+                        city,
+                        country
+                        ]
+                print(row)
+                self.sheet_rows.append(row)
             time.sleep(3)
 
 
@@ -45,7 +73,6 @@ class HermitCrab(object):
                 username=os.environ.get('MONGO_USER'),
                 password=os.environ.get('MONGO_PASSWORD'))
             user = model.User.objects(fb_userId=self.fb_user_id).get()
-            print(user)
             return user.fb_accessToken
         except Exception as ex:
             print(ex)
@@ -57,10 +84,8 @@ class HermitCrab(object):
 
         fields = str('?fields=about,category,contact_address,engagement,'
         'emails,location,phone&access_token=')
-        print("%s/%s%s%s" % (self.graph, page, fields,
-            self.access_token))
+        #print("%s/%s%s%s" % (self.graph, page, fields,
+        #    self.access_token))
         r = requests.get("%s/%s%s%s" % (self.graph, page, fields,
             self.access_token))
-        print(r.text)
-        print(r.json())
-        return None
+        return r.json()
