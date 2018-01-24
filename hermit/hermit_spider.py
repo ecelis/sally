7f1d2078741f4a951297fe8d50611bf5a598bfb3
Ernesto Celis <celisdelafuente@gmail.com>
2018-01-24 11:48:52 -0600

    WIP hermit autoscraps

--- hermit/hermit_spider.py
+++ hermit/hermit_spider.py
@@ -1,12 +1,14 @@
 import os
 import re
+import time
 import logging
 import requests
 from mongoengine import connect
-import model
+import hermit.model as model
 import sally.google.spreadsheet as gs
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 class HermitCrab(object):
@@ -17,12 +19,20 @@ class HermitCrab(object):
         self.score = gs.get_score()
         self.fb_user_id = fb_user_id
         self.access_token = self.get_token()
+        print(self.access_token)
         self.graph = 'https://graph.facebook.com'
 
         lines = ["%s" % str(l).rstrip() for l in gs.get_urls(source_file)]
         fb = re.compile(r'facebook', re.IGNORECASE)
         self.start_urls = list(filter(fb.search, list(filter(None, lines))))
+
         print(self.start_urls)
+        print(len(self.start_urls))
+
+        for item in self.start_urls:
+            print(item.split('/')[1])
+            self.parse_item(item.split('/')[1])
+            time.sleep(3)
 
 
     def get_token(self):
@@ -35,8 +45,10 @@ class HermitCrab(object):
                 username=os.environ.get('MONGO_USER'),
                 password=os.environ.get('MONGO_PASSWORD'))
             user = model.User.objects(fb_userId=self.fb_user_id).get()
+            print(user)
             return user.fb_accessToken
-        except Exception:
+        except Exception as ex:
+            print(ex)
             return None
 
 
@@ -46,7 +58,9 @@ class HermitCrab(object):
         fields = str('?fields=about,category,contact_address,engagement,'
         'emails,location,phone&access_token=')
         print("%s/%s%s%s" % (self.graph, page, fields,
-            user.fb_accessToken))
+            self.access_token))
         r = requests.get("%s/%s%s%s" % (self.graph, page, fields,
-            user.fb_accessToken))
+            self.access_token))
+        print(r.text)
+        print(r.json())
         return None
