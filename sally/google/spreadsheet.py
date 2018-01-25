db74f0835ee57e5b9128b7763db9d58653d46900
Ernesto Celis <celisdelafuente@gmail.com>
2018-01-25 16:45:36 -0600

    UPDATE PEP cleanup

--- sally/google/spreadsheet.py
+++ sally/google/spreadsheet.py
@@ -1,6 +1,5 @@
+"""Google spreadsheets interface for Sally crawler."""
 import os
-import sys
-import datetime
 import logging
 from sally.google import authorize
 
@@ -10,8 +9,9 @@ logger = logging.getLogger(__name__)
 def get_spreadsheet(spreadsheetId):
     """Return an existing spreadsheet given by the ID."""
     service = authorize.get_service('sheets', 'v4')
-    request = service.spreadsheets().get(spreadsheetId=spreadsheetId, ranges=[],
-            includeGridData=False)
+    request = service.spreadsheets().get(
+        spreadsheetId=spreadsheetId,
+        ranges=[], includeGridData=False)
     response = request.execute()
     return response
 
@@ -25,7 +25,7 @@ def create_spreadsheet(title):
         }
     }
 
-    request = service.spreadsheets().create(body = body_)
+    request = service.spreadsheets().create(body=body_)
     response = request.execute()
     return response
 
@@ -33,25 +33,30 @@ def create_spreadsheet(title):
 def create_sheet(spreadsheetId, title):
     """Return a new sheet with given _title_ at given spreadsheet ID."""
     body = {
-            "addSheet": {
-                "properties": {
-                    "title": title,
-                    "gridProperties": {
-                        "rowCount": 100,
-                        "columnCount": 9,
-                        "frozenRowCount": 1
-                        }
+        "addSheet": {
+            "properties": {
+                "title": title,
+                "gridProperties": {
+                    "rowCount": 100,
+                    "columnCount": 9,
+                    "frozenRowCount": 1
                     }
                 }
             }
+        }
     service = authorize.get_service('sheets', 'v4')
-    request = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId,
-            body={ 'requests': [body] })
+    request = service.spreadsheets().batchUpdate(
+        spreadsheetId=spreadsheetId,
+        body={'requests': [body]})
     response = request.execute()
     return response
 
 
-def insert_to(spreadsheetId, sheet, rows=[['','','','','','','','']], offset=1):
+def insert_to(
+        spreadsheetId,
+        sheet,
+        rows=[['', '', '', '', '', '', '', '']],
+        offset=1):
     """
     Insert a new item in a google spreadsheet. Return updated spreadshet.
 
@@ -59,36 +64,36 @@ def insert_to(spreadsheetId, sheet, rows=[['','','','','','','','']], offset=1):
     spreadsheetId - ID of target spreadsheet
     row - I.E. [SCORE,URL,OFFER,META,TELEPHONE,EMAIL,ECOMMERCE,SHOPING CART,
         SOCIAL NETWORKS, LOCATION, CRAWLING DATE]
-    offsset - row number to skip in the preadsheet before insert, like in headers
+    offsset - row number to skip in spreadsheet before insert, like in headers
     """
     service = authorize.get_service('sheets', 'v4')
     rangeName = sheet + '!A' + str(offset)
-    majorDimension = 'ROWS'
-    value_input_option = 'RAW' ## USER_ENTERED
-    today = datetime.datetime.now().strftime('%m/%d/%Y')
-    body = { 'values': rows }
+    value_input_option = 'RAW'
+    body = {'values': rows}
     response = service.spreadsheets().values().update(
         spreadsheetId=spreadsheetId, range=rangeName,
         valueInputOption=value_input_option, body=body).execute()
     return response
 
 
-def get_settings(spreadsheetId = os.environ['SALLY_SETTINGS_ID']):
+def get_settings(spreadsheetId=os.environ['SALLY_SETTINGS_ID']):
     """Return crawler settings from given spreadsheet ID."""
     range_ = 'settings!A1:F1000'
     service = authorize.get_service('sheets', 'v4')
-    request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId,
-            range=range_, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE',
-            dateTimeRenderOption='FORMATTED_STRING')
+    request = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId,
+        range=range_, majorDimension='COLUMNS',
+        valueRenderOption='UNFORMATTED_VALUE',
+        dateTimeRenderOption='FORMATTED_STRING')
     response = request.execute()
     settings = {
-            "allowed_domains": response['values'][0][1:],
-            "disallowed_domains": response['values'][1][1:],
-            "allowed_keywords": response['values'][2][1:],
-            "disallowed_keywords": response['values'][3][1:],
-            "networks": response['values'][4][1:],
-            "ecommerce": response['values'][5][1:]
-            }
+        "allowed_domains": response['values'][0][1:],
+        "disallowed_domains": response['values'][1][1:],
+        "allowed_keywords": response['values'][2][1:],
+        "disallowed_keywords": response['values'][3][1:],
+        "networks": response['values'][4][1:],
+        "ecommerce": response['values'][5][1:]
+        }
     return settings
 
 
@@ -97,9 +102,12 @@ def get_urls(spreadsheetId):
     range_ = 'A1:A1000'
     try:
         service = authorize.get_service('sheets', 'v4')
-        request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId,
-                range=range_, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE',
-                dateTimeRenderOption='FORMATTED_STRING')
+        request = service.spreadsheets().values().get(
+            spreadsheetId=spreadsheetId,
+            range=range_,
+            majorDimension='COLUMNS',
+            valueRenderOption='UNFORMATTED_VALUE',
+            dateTimeRenderOption='FORMATTED_STRING')
         response = request.execute()
         logger.debug(response['values'][0])
         return response['values'][0]
@@ -108,23 +116,26 @@ def get_urls(spreadsheetId):
         return []
 
 
-def get_score(spreadsheetId = os.environ['SALLY_SETTINGS_ID']):
+def get_score(spreadsheetId=os.environ['SALLY_SETTINGS_ID']):
     """Return score values from given Google spreadsheet ID."""
     range_ = 'score!A2:B1000'
     service = authorize.get_service('sheets', 'v4')
-    request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId,
-            range=range_, majorDimension='ROWS', valueRenderOption='UNFORMATTED_VALUE',
-            dateTimeRenderOption='FORMATTED_STRING')
+    request = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId,
+        range=range_,
+        majorDimension='ROWS',
+        valueRenderOption='UNFORMATTED_VALUE',
+        dateTimeRenderOption='FORMATTED_STRING')
     response = request.execute()
     logger.debug(response)
     score = {
-            "email": response['values'][0][1],
-            "telephone": response['values'][1][1],
-            "ecommerce": response['values'][2][1],
-            "offer": response['values'][3][1],
-            "network": response['values'][4][1],
-            "secure_url": response['values'][5][1],
-            "cart": response['values'][6][1],
-            "likes": response['values'][7][1]
-            }
+        "email": response['values'][0][1],
+        "telephone": response['values'][1][1],
+        "ecommerce": response['values'][2][1],
+        "offer": response['values'][3][1],
+        "network": response['values'][4][1],
+        "secure_url": response['values'][5][1],
+        "cart": response['values'][6][1],
+        "likes": response['values'][7][1]
+        }
     return score
