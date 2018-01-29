99821694aaa8d1bb60f056065f6b8d6e377903f4
Ernesto Celis <celisdelafuente@gmail.com>
2018-01-29 10:57:30 -0600

    UPDTE hermit web shell

--- hermit/app.py
+++ hermit/app.py
@@ -1,3 +1,4 @@
+"""Facebook web shell for hermit."""
 import os
 import logging
 import cherrypy
@@ -7,85 +8,65 @@ import model
 
 logger = logging.getLogger(__name__)
 
-class HermitShell(object):
 
+class HermitShell(object):
+    """Facebook web shell for Hermit"""
     def __init__(self):
         self.graph = 'https://graph.facebook.com'
 
-
-    def get_long_ttl_token(self, accessToken):
-        r = requests.get("%s/oauth/access_token?grant_type=fb_exchange_token"
-                "&client_id=%s&client_secret=%s&fb_exchange_token=%s"
-                % (self.graph,
-                    os.environ.get('FACEBOOK_APP_ID'),
-                    os.environ.get('FACEBOOK_APP_SECRET'),
-                    accessToken))
-        return r.json()
-
-
-    @cherrypy.expose
-    def index(self):
-        return open('index.html')
-
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def page(self, page, fb_user_id):
-
-        fields = str('?fields=about,category,contact_address,engagement,'
-        'emails,location,phone&access_token=')
-
+    def mongo_connect(self):
+        """Ëstablish a MongoDB connetion"""
         try:
             connect(os.environ.get('MONGO_DBNAME'),
                     host="mongodb://" + os.environ.get('MONGO_HOST'),
                     port=int(os.environ.get('MONGO_PORT')),
                     replicaset=os.environ.get('MONGO_REPLICA_SET'),
-                username=os.environ.get('MONGO_USER'),
-                password=os.environ.get('MONGO_PASSWORD'))
+                    username=os.environ.get('MONGO_USER'),
+                    password=os.environ.get('MONGO_PASSWORD'))
         except Exception:
-            cherrypy.error("[page] Can't connect to MongoDB", traceback=True)
-            return {'status': 500, 'statusText': "Can't connect to MongoDB"}
+            cherrypy.log("[authorize]", traceback=True)
+            return {'status': 500, 'statusText': 'Can\'t connect to MongoDB'}
 
-        try:
-            user = model.User.objects(fb_userId=fb_user_id).get()
-        except:
-            cherrypy.error("[page] Can't get Facebook user token",
-                    traceback=True)
-            return {'status': 500, 'statusText': "Can't get Facebook user token"}
-        r = requests.get("%s/%s%s%s" % (self.graph, page, fields,
-            user.fb_accessToken))
-        cherrypy.log(r.text)
-        cherrypy.log('===')
-        print(r.json())
-        return {'status': 200, 'statusText': 'algo'}
+    def get_long_ttl_token(self, accessToken):
+        """Exchange short lived Facebook user token for long lived one"""
+        request = requests.get(
+            "%s/oauth/access_token?grant_type=fb_exchange_token"
+            "&client_id=%s&client_secret=%s&fb_exchange_token=%s"
+            % (self.graph,
+               os.environ.get('FACEBOOK_APP_ID'),
+               os.environ.get('FACEBOOK_APP_SECRET'),
+               accessToken))
+        return request.json()
 
+    @cherrypy.expose
+    def index(self):
+        """Return landing page"""
+        return open('index.html')
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def page(self, page, fb_user_id):
+        pass
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def authorize(self):
+        """Authorize or register user with Facebook account"""
         data = cherrypy.request.json
         logger.debug(data)
-        try:
-            connect(os.environ.get('MONGO_DBNAME'),
-                    host="mongodb://" + os.environ.get('MONGO_HOST'),
-                    port=int(os.environ.get('MONGO_PORT')),
-                    replicaset=os.environ.get('MONGO_REPLICA_SET'),
-                username=os.environ.get('MONGO_USER'),
-                password=os.environ.get('MONGO_PASSWORD'))
-        except Exception:
-            cherrypy.log("[authorize]", traceback=True)
-            return {'status': 500, 'statusText': 'Can\'t connect to MongoDB'}
-
+        self.mongo_connect()
 
         try:
-            user = model.User.objects(email=data['email'], fb_userId=data['fb_userId']).get()
+            user = model.User.objects(
+                email=data['email'],
+                fb_userId=data['fb_userId']).get()
         except:
             user = model.User(
-                    name = data['name'],
-                    email = data['email'],
-                    fb_userId = data['fb_userId'],
-                    fb_accessToken = data['fb_accessToken'])
+                name=data['name'],
+                email=data['email'],
+                fb_userId=data['fb_userId'],
+                fb_accessToken=data['fb_accessToken'])
             user.save()
 
         token = self.get_long_ttl_token(user.fb_accessToken)
@@ -96,6 +77,6 @@ class HermitShell(object):
 
 if __name__ == '__main__':
     config = {
-            '/': {'server.socket_host': '0.0.0.0'}
-            }
+        '/': {'server.socket_host': '0.0.0.0'}
+        }
     cherrypy.quickstart(HermitShell(), '/', "app.conf")
