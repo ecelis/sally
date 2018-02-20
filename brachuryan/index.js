901c08a8859ae040e1291b729348c3a9c29cfe55
Ernesto Celis <celisdelafuente@gmail.com>
2018-02-20 16:44:56 -0600

    FB login and search by parameters.
    -q Query
    -n Category name
    -a Filter args

--- brachuryan/index.js
+++ brachuryan/index.js
@@ -5,13 +5,21 @@ if (process.argv.length < 7) {
   process.exit(1);
 }
 
-
-const FACEBOOK = 'https://www.facebook.com/search/pages/';
+const FACEBOOK = 'https://www.facebook.com/';
+const LOGIN = 'login';
+const SEARCH = 'search/pages/';
 const QUERY = '?q=';
 const FILTER_CATEGORY = '&filters_category=';
+const LOGIN_BUTTON = 'button#loginbutton';
+const USERNAME_FIELD = 'input#email';
+const PASSWORD_FIELD = 'input#pass';
+
+function login() {
+  return FACEBOOK + LOGIN;
+};
 
 function buildQuery(keyWord, filterName, filterArgs) {
-  return FACEBOOK
+  return FACEBOOK + SEARCH
     + QUERY + keyWord
     + FILTER_CATEGORY + '{"name"%3a"' + filterName
     + '"%2c"args"%3a"' + filterArgs + '"}';
@@ -20,13 +28,22 @@ function buildQuery(keyWord, filterName, filterArgs) {
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    slowMo: 250,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
   let url = buildQuery(process.argv[3], process.argv[5], process.argv[7]);
+  await page.goto(login(), {
+    waitUntil: 'networkidle2'
+  });
+  await page.waitForSelector(LOGIN_BUTTON);
+  await page.focus(USERNAME_FIELD);
+  await page.type(USERNAME_FIELD, process.env.FB_USERNAME);
+  await page.focus(PASSWORD_FIELD);
+  await page.type(PASSWORD_FIELD, process.env.FB_PASSWD);
+  await page.click(LOGIN_BUTTON);
   await page.goto(url);
   await page.screenshot({path: '/tmp/example.png'});
-
 
   //  await browser.close();
 })();
